f043f47bd23856f65da496d04f3e250ec188b83e
ARYAN GUPTA <aryan9090900@gmail.com>
2025-07-01 15:27:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 
 export default function HomePage() {
-  const cookieStore = cookies();
-  const token = cookieStore.get('next-auth.session-token')?.value;
-
+  const cookieStore = cookies()
+  
+const token = cookieStore.get('next-auth.session-token')?.value ||
+    cookieStore.get('__Secure-next-auth.session-token')?.value
+  
   if (!token) {
     redirect('/login');
   } else {
