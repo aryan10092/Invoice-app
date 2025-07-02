ca618b4e9f028995b8ff9c19ce89d9c11ff1fa95
ARYAN GUPTA <aryan9090900@gmail.com>
2025-07-02 21:09:51 +0530

    Update layout.js

--- app/(all pages)/layout.js
+++ app/(all pages)/layout.js
@@ -13,7 +13,7 @@ export default function DashboardLayout({ children }) {
     <div className="min-h-screen bg-gray-50">
       <Sidebar open={sidebarOpen} close={closeSidebar} />
       <div className="lg:pl-64">
-        <Headertoggle={toggleSidebar} />
+        <Headertoggle toggle={toggleSidebar} />
         <main className="p-6">
           {children}
         </main>
