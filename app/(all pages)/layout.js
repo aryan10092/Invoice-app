d69e00518a57d2a7f5b0963349b694c27ecc7a74
ARYAN GUPTA <aryan9090900@gmail.com>
2025-07-02 21:07:12 +0530

    Update layout.js

--- app/(all pages)/layout.js
+++ app/(all pages)/layout.js
@@ -1,12 +1,19 @@
+'use client'
+import { useState } from 'react';
 import Sidebar from '@/components/dashboard/Sidebar';
 import Header from '@/components/dashboard/Header';
 
 export default function DashboardLayout({ children }) {
+
+   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const toggleSidebar = () => setSidebarOpen((open) => !open)
+  const closeSidebar = () => setSidebarOpen(false)
+  
   return (
     <div className="min-h-screen bg-gray-50">
-      <Sidebar />
+      <Sidebar open={sidebarOpen} close={closeSidebar} />
       <div className="lg:pl-64">
-        <Header />
+        <Headertoggle={toggleSidebar} />
         <main className="p-6">
           {children}
         </main>
